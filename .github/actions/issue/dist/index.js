e61e8115486e5801af745fa491333a759d792b4b
Daniel Michaelis <daniel.michaelis@ms.ingenico.com>
2022-02-28 15:49:42 +0100

    fix: replace octokit.issues.create by octokit.rest.issues.create

--- .github/actions/issue/dist/index.js
+++ .github/actions/issue/dist/index.js
@@ -5092,7 +5092,7 @@ module.exports = /******/ (function (modules, runtime) {
           const assignees = core.getInput("assignees");
 
           const octokit = new github.getOctokit(token);
-          const response = await octokit.issues.create({
+          const response = await octokit.rest.issues.create({
             //owner: github.context.repo.owner,
             //repo: github.context.repo.repo,
             ...github.context.repo,
